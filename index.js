b1bf949d00177c691b25f43a53b92d5a0120d999
Jay Zehngebot <jay.zehngebot@gmail.com>
2016-05-27 09:21:53 -0400

    first return sticker

--- index.js
+++ index.js
@@ -13,6 +13,71 @@ app.get('/', function(req, res){
   res.send('It works!');
 });
 
+
+app.get('/:sticker', function(req, res){
+
+  console.log('incoming request');
+  console.log(req.params.sticker);
+
+  var stickerKeyword = req.params.sticker;
+
+  // var data = JSON.parse(body);
+
+  var parsed_url = url.format({
+    pathname: 'http://www.comicdrop.com/api/v1/stickers',
+    query: {
+      keywords: stickerKeyword
+    }
+  })
+
+  console.log('parsed_url : ', parsed_url);
+
+
+  request(parsed_url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+
+      console.log('data', data);
+
+      // get sticker id 
+
+      var firstStickerID = data.stickers[0].id;
+      console.log('firstStickerID', firstStickerID);
+
+
+      // 'http://www.comicdrop.com/api/v1/stickers/:id/:size'
+      // format new request for sticker image 
+      // var sticker_url = url.format({
+      //   pathname: 'http://www.comicdrop.com/api/v1/stickers/',
+      //   id: firstStickerID
+      // })
+
+      var sticker_url = 'http://www.comicdrop.com/api/v1/stickers/';
+
+      sticker_url += firstStickerID;
+      sticker_url += '/thumbnail';
+
+        console.log('sticker_url : ', sticker_url);
+
+        // request(sticker_url, function(err, response,body){
+        //   if (!error && response.statusCode == 200) {
+        //       console.log('success what is body ', body);
+        //   }
+        // })
+
+      // var first_url = data.response.hits[0].result.url;
+
+      var body = {
+        response_type: "in_channel",
+        text: sticker_url
+      };
+
+      res.send(body);
+    }
+  });
+
+});
+
 app.post('/post', function(req, res){
   var parsed_url = url.format({
     pathname: 'https://api.genius.com/search',
